f121515b0cd85ff71fb1a14cd4a927b9738aa5ef
Jorge Eduardo <jeduoliveira81@gmail.com>
2021-10-05 17:50:35 -0300

    adicionando versão 14.1.0

--- centos/6/12.2.0.200122-FreeVersion/config.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM lum_CfgEnvironmentConf;
-
-INSERT INTO lum_CfgEnvironmentConf (id, type, fileSystemImplementation, javaMelodyEnabled, bigDataRepositoryType, esClusterName,esConnectionAddresses,esHttpConnectionAddresses, esBulkConcurrentRequests, useUserGroupsSessionCache) 
-VALUES(  '000000000A00000000000A0000100000', 'DEVELOPMENT','lumis.portal.filesystem.impl.SingleCopyFileSystem', 1,'ELASTICSEARCH_TRANSPORT_CLIENT', 'ELASTICSEARCH_CLUSTER_NAME', 'ELASTICSEARCH_HOST:9300', 'ELASTICSEARCH_HOST:9200', 10,1);
-
-UPDATE lum_Website set webRootPath='WEB_ROOT_PATH' where name ='default';
-
-COMMIT;
